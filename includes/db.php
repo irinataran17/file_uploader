cf49aa77158778c7cda30c93e03ea2915b1b4959
irinataran17 <irina.taran17@gmail.com>
2019-03-26 18:58:12 +0200

    initial commit

---
+++ includes/db.php
@@ -0,0 +1,13 @@
+<?php
+
+$db_host = 'localhost';
+$db_name = 'file_uploader';
+$db_user = 'uploader';
+$db_pass = 'RakpLx61zJSLds0Q';
+
+$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+
+if (mysqli_connect_error()) {
+    echo mysqli_connect_error();
+    exit;
+}
---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+require 'includes/db.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>File uploader</title>
+</head>
+<body>
+    <H1>Hey, welcome to the file uploader!</H1>
+    <p>Some bla bla bla staff here regarding the restrictions</p>
+
+    <form action="">
+    <input type="file" name="fileupload" value="fileupload">
+    
+    <input type="submit" value="submit">
+    </form>
+
+
+</body>
+</html>
