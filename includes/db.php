61e7d9c1d98dc81951489a4e78ced9207ce6d787
irinataran17 <irina.taran17@gmail.com>
2019-03-27 15:37:30 +0200

    Added restrictions for the file type/size, error handlers added

--- includes/db.php
+++ includes/db.php
@@ -1,13 +1,18 @@
 <?php
 
-$db_host = 'localhost';
-$db_name = 'file_uploader';
-$db_user = 'uploader';
-$db_pass = 'RakpLx61zJSLds0Q';
+function getDB() {
+    
+    $db_host = 'localhost';
+    $db_name = 'file_uploader';
+    $db_user = 'uploader';
+    $db_pass = 'RakpLx61zJSLds0Q';
 
-$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+    $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 
-if (mysqli_connect_error()) {
-    echo mysqli_connect_error();
-    exit;
+    if (mysqli_connect_error()) {
+        echo mysqli_connect_error();
+        exit;
+    }
+
+    return $conn;
 }
--- index.php
+++ index.php
@@ -2,6 +2,44 @@
 
 require 'includes/db.php';
 
+$conn = getDB();
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    var_dump($_FILES);
+
+    try {
+
+        if (empty($_FILES)) {
+            throw new Exception('Invalid upload');
+        }
+
+        switch ($_FILES['file']['error']) {
+            case UPLOAD_ERR_OK:
+                break;
+            case UPLOAD_ERR_NO_FILE:
+                throw new Exception('No file uploaded');
+                break;
+            case UPLOAD_ERR_INI_SIZE:
+                throw new Exception('File is too large from the server settings');
+                break;
+            default:
+                throw new Exception('An error occured');
+        }
+
+        $mime_types = ['image/gif', 'image/png', 'image/jpeg', 'text/csv', 'text/html', 'text/plain', 'application/msword'];
+
+        $finfo = finfo_open(FILEINFO_MIME_TYPE);
+        $mime_type = finfo_file($finfo, $_FILES['file']['tmt_name']);
+
+        if (!in_array($mime_type, $mime_types)) {
+            throw new Exception('Invalid file type');
+        }
+
+    } catch (Exception $e) {
+        echo $e;
+    }
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -14,14 +52,18 @@ require 'includes/db.php';
 </head>
 <body>
     <H1>Hey, welcome to the file uploader!</H1>
-    <p>Some bla bla bla staff here regarding the restrictions</p>
+    <p>Please note that you can upload files up to 5M. Here another bla bla bla staff regarding the restrictions</p>
 
-    <form action="">
-    <input type="file" name="fileupload" value="fileupload">
-    
-    <input type="submit" value="submit">
+    <form method="post" enctype="multipart/form-data">
+
+        <div>
+            <label for="file">File to be selected</label>
+            <input type="file" name="file" id="file">
+        </div>
+
+        <button>Upload</button>
+
     </form>
-
 
 </body>
 </html>
